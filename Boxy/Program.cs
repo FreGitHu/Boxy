4fd7d6f95b327e8849e2b3b30b194f9c10059e7d
Freddie <freddiejscott@outlook.com>
2023-01-14 16:13:28 +0000

    Add project files.

---
+++ Boxy/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+		try
+		{
+			if (args[1] == "--help")
+			{
+				Console.WriteLine("The Boxy Programming Language")
+			}
+
+		}
+		catch (IndexOutOfRangeException)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("Not enough arguments specifed");
+			Environment.Exit(1);
+			throw;
+		}
+    }
+}
